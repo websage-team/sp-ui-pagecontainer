a5d8b6d4f50257252d20215a1e2dab4f054a8678
Diablohu <Diablohu@users.noreply.github.com>
2018-05-28 16:50:01 +0800

    优化浏览器中的表现

--- index.jsx
+++ index.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 // import { store } from 'super-project/ReactApp'
 
+let currentMetaTags
+
 export default (funcGetPageInfo) => (WrappedComponent) => {
     const getInfo = (store) => {
         if (typeof funcGetPageInfo !== 'function') return
@@ -26,6 +28,8 @@ export default (funcGetPageInfo) => (WrappedComponent) => {
         }
 
         componentDidMount() {
+            if (__SERVER__) return
+
             const infos = getInfo(this.context.store)
 
             // 替换页面标题
@@ -33,19 +37,46 @@ export default (funcGetPageInfo) => (WrappedComponent) => {
 
             // 替换 metas
             const head = document.getElementsByTagName('head')[0]
-            head.innerHTML = head.innerHTML.replace(
-                /<!--SUPER_METAS_START-->(.*?)<!--SUPER_METAS_END-->/g,
-                `<!--SUPER_METAS_START-->${infos.metas
-                    .filter(meta => typeof meta === 'object')
-                    .map(meta => {
-                        let str = '<meta'
-                        for (var key in meta) {
-                            str += ` ${key}="${meta[key]}"`
-                        }
-                        str += '>'
-                        return str
-                    }).join('')}<!--SUPER_METAS_END-->`
-            )
+            if (!Array.isArray(currentMetaTags)) {
+                // const exec = /<!--SUPER_METAS_START-->(.*?)<!--SUPER_METAS_END-->/g.exec(head.innerHTML)
+                // if (Array.isArray(exec) && exec.index) {
+                //     console.log(exec)
+                // }
+                // head.innerHTML = head.innerHTML.replace(
+                //     /<!--SUPER_METAS_START-->(.*?)<!--SUPER_METAS_END-->/g,
+                //     `<!--SUPER_METAS_START-->${infos.metas
+                //         .filter(meta => typeof meta === 'object')
+                //         .map(meta => {
+                //             let str = '<meta'
+                //             for (var key in meta) {
+                //                 str += ` ${key}="${meta[key]}"`
+                //             }
+                //             str += '>'
+                //             return str
+                //         }).join('')}<!--SUPER_METAS_END-->`
+                // )
+                head.innerHTML = head.innerHTML.replace(
+                    /<!--SUPER_METAS_START-->(.*?)<!--SUPER_METAS_END-->/g,
+                    ''
+                )
+                currentMetaTags = []
+            }
+
+            currentMetaTags.forEach(el => {
+                el.parentNode.removeChild(el)
+            })
+            currentMetaTags = []
+
+            infos.metas
+                .filter(meta => typeof meta === 'object')
+                .forEach(meta => {
+                    const el = document.createElement('meta')
+                    for (var key in meta) {
+                        el.setAttribute(key, meta[key])
+                    }
+                    head.appendChild(el)
+                    currentMetaTags.push(el)
+                })
         }
 
         render = () => <WrappedComponent {...this.props} />
